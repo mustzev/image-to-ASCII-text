a897c3cff825d1b84baebc1d9cfd02657b5e00f9
Sukhbold Altangerel <col.de.staz@gmail.com>
2019-12-30 15:17:22 +0800

    Update ASCII_img.py

--- ASCII_img/ASCII_img.py
+++ ASCII_img/ASCII_img.py
@@ -1,17 +1,21 @@
 from PIL import Image
 
+# Replace "girl.png" with your image within the directory.
 im = Image.open("girl.png")
 im = im.resize((int(im.width / 2), int(im.height / 4)))
 
 width, height = im.size
 
+# ASCII text is gonna be created using #, $, and @.
+# You can change these to any symbols you want for customization.
 ASCII_char = ['#', '$', '@']
 ASCII_text = ''
 
 max_brightness = 0
 min_brightness = 255
 
-
+# Here we are finding the brigthest, and the darkest pixel's brightness in the given image.
+# So later we can assign the each ASCII symbols to a pixel depending on it's relative brightness.
 for w in range(width):
     for h in range(height):
         rgb = im.getpixel((w, h))
@@ -23,7 +27,9 @@ for w in range(width):
         if brightness < min_brightness:
             min_brightness = brightness
 
-
+# Here comes the creating ASCII text part.
+# If the given pixel is darker lets assign the first ASCII symbol. And so on ...
+# Also adding a new line to the ASCII text everytime the loop is at the end of a width.
 for h in range(height):
     for w in range(width):
         rgb1 = im.getpixel((w, h))
@@ -39,6 +45,7 @@ for h in range(height):
         if w == width-1:
             ASCII_text += '\n'
 
+# Printing the ASCII text to the terminal.
 print(ASCII_text)
 
 
